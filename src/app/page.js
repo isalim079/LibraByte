b65933d498338739f25fb3998e3a21cd985f87e8
isalim079 <salimreza1025@gmail.com>
2024-01-15 21:07:36 +0600

    added font

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className='text-3xl text-center'>LibraByte</h1>
+      <h1 className='text-3xl text-center font-poppins'>LibraByte</h1>
     </div>
   )
 }
