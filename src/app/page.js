8f054bbc9ac423a50f24d5c00708383f24646657
isalim079 <salimreza1025@gmail.com>
2024-01-15 21:02:47 +0600

    default added

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,6 @@ export default function Home() {
   return (
     <div>
       <h1 className='text-3xl text-center'>LibraByte</h1>
-<<<<<<< HEAD
-      <p>This will be your home page and main Brunch ch</p>
-=======
-      <h1>Samin branch</h1>
-      <p> Salim page</p>
-      <p>This is development</p>
-      <p>This will be your home page and main Brunch</p>
-      <p>This is brunch</p>
-      <p>This will be your home page</p>
-      <p>nishat</p>
->>>>>>> fa897031e785f1be0b4b5d4fa291afe5b5ad6b28
     </div>
   )
 }
