2f6c1496578767899f2ca6ba0fee004dc5f7a480
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-01-16 23:06:32 +0600

    Navbar added

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,16 @@
+import Navbar from '@/components/Navbar/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1 className='text-3xl text-center'>LibraByte</h1>
-<<<<<<< HEAD
       <h1>Samin branch</h1>
       <p> Salim page</p>
       <p>This is development Brunch</p>
-=======
       <p>This will be your home page</p>
       <p>nishat</p>
->>>>>>> 189c797dc68b05b403108f1aa9f5a0f0adc4adf5
     </div>
   )
 }
