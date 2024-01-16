cdcc4975d4e29d0771273164329de385c7a1bfb3
nadira1187 <nadira35-1187@s.diu.edu.bd>
2024-01-17 00:23:47 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
-
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <h1 className='text-3xl text-center'>LibraByte</h1>
-      <h1>Samin branch</h1>
-      <p> Salim page</p>
-      <p>This is development Brunch</p>
       <p>This will be your home page and main Brunch</p>
-      <p>This is brunch</p>
-      <p>This will be your home page</p>
-      <p>nishat</p>
     </div>
   )
 }
