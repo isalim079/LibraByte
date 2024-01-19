3efa60079ab0018bd4a51f938e675757115912f8
isalim079 <salimreza1025@gmail.com>
2024-01-19 15:48:03 +0600

    coming soon added

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -7,7 +7,7 @@ import comingSoon from "@/assets/animation/comingSoon.json"
 const Blog = () => {
     return (
         <div className="flex justify-center items-center h-screen">
-            <Lottie animationData={comingSoon} loop={true} className="w-80"/>
+            <Lottie animationData={comingSoon} loop={true} className="w-72 -z-10"/>
         </div>
     );
 };
