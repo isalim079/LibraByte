28890fe9b50a2588308396f7e959f751dfc14d1b
isalim079 <salimreza1025@gmail.com>
2024-01-21 16:39:29 +0600

    devided in author talks

--- src/components/Blog/MiddleSideSection/AuthorTalks/AuthorTalks.js
+++ src/components/Blog/MiddleSideSection/AuthorTalks/AuthorTalks.js
@@ -3,12 +3,17 @@
 const AuthorTalks = () => {
     return (
         <div className="mt-8 p-8 bg-lightWhite rounded-md drop-shadow-lg">
+            {/* title section */}
             <div>
                 <h1 className="text-2xl font-semibold text-oliveGreen">Author Talks</h1>
                 <div className="border-2 border-oliveGreen w-32 mt-1 mb-5"></div>
             </div>
+
+            {/* author section */}
             <div>
-                
+                <div>
+
+                </div>
             </div>
         </div>
     );
