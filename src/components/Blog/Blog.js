50c0a8ea9df98fdaa7ad35842c73b82c3ef4c8b3
isalim079 <salimreza1025@gmail.com>
2024-01-21 16:24:48 +0600

    text area added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,32 +1,22 @@
 "use client";
 
 import LeftSideSection from "./LeftSideSection/LeftSideSection";
+import MiddleSideSection from "./MiddleSideSection/MiddleSideSection";
 
 const Blog = () => {
     return (
         <div className="max-w-screen-xl mx-auto mt-10 mb-10">
-
             {/* Full blog container */}
+
             <div className="grid grid-cols-3 gap-10">
-
                 {/* Left side section */}
+
                 <div className="">
-
-                    {/* search section */}
-                    <div><LeftSideSection /></div>
-
-                    {/* category section */}
-                    <div></div>
-
-                    {/* Top post section */}
-                    <div></div>
-
-                    {/* Instagram section */}
-                    <div></div>
+                    <LeftSideSection />
                 </div>
 
                 {/* Middle section */}
-                <div className="border border-black">post section 1</div>
+                <div className="border border-black"><MiddleSideSection /></div>
 
                 {/* Right section */}
                 <div className="border border-black">port section 2</div>
---
+++ src/components/Blog/MiddleSideSection/MiddleSideSection.js
@@ -0,0 +1,12 @@
+import PostField from "./PostField/PostField";
+
+
+const MiddleSideSection = () => {
+    return (
+        <div>
+            <PostField />
+        </div>
+    );
+};
+
+export default MiddleSideSection;
