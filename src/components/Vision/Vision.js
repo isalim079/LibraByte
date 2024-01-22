f0f393a1884ef65c6d294d0341760c3017afdaef
isalim079 <salimreza1025@gmail.com>
2024-01-22 19:00:15 +0600

    our values edited

--- src/components/Vision/Vision.js
+++ src/components/Vision/Vision.js
@@ -9,7 +9,7 @@ const Vision = () => {
         <h2 className="text-4xl text-white font-bold mb-2 mt-4 lg:mt-0">Our Vision</h2>
         <p className="text-sm text-white text-center mb-3 p-2 md:p-0 ">Our goal is to create an efficient and user-friendly Library Management System that empowers <br/> librarians with advanced tools for cataloging, organizing, and tracking library resources. </p>
         
-           <div className="grid grid-cols-1 gap-3 ml-8 md:ml-6  md:grid-cols-3 lg:grid-cols-4 lg:ml-6 my-6 items-center ">
+           <div className="grid grid-cols-1 gap-3 md:gap-16 ml-8 md:ml-6  md:grid-cols-3 lg:grid-cols-4 lg:ml-6 my-6 items-center ">
             <div className=" card justify-center items-center card-body w-56 h-52 bg-white p-5">
             <Image className=" w-16"
                        src="https://i.ibb.co/S0rpCnw/global-975645.png"
