f34482ec3b84b92c9c862a80e6f2ba73019fa549
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-01-23 19:37:44 +0600

    some added in about us

--- src/app/aboutUs/page.js
+++ src/app/aboutUs/page.js
@@ -1,12 +1,17 @@
-import Vision from '@/components/Vision/Vision';
-import OurValues from '@/components/OurValues/OurValues';
+import Vision from '@/components/AboutUsPage/Vision/Vision';
+import OurValues from '@/components/AboutUsPage/OurValues/OurValues';
 import React from 'react';
+import ContactUs from '@/components/AboutUsPage/ContactUs/ContactUs';
+import Faq from '@/components/AboutUsPage/FAQ/Faq';
 
 const AboutUs = () => {
     return (
         <div>
             <Vision></Vision>
             <OurValues></OurValues>
+            <Faq />
+            <ContactUs />
+
         </div>
     );
 };
