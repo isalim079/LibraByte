43f905eaf7784c4f65a19e4949b94c9d603eaf15
alif0013 <mdalifh36@gmail.com>
2024-01-24 15:59:01 +0600

    added user dashboard

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -4,7 +4,21 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
+            {/* jokhn backend api and jwt verify implement kora ses hoye jabe tokhn ei login kaj korbe   */}
+           
+            {/* {
+                isAdmin ?
+
+                 <AdminDashboard></AdminDashboard> 
+                :
+                 <UserDashboard></UserDashboard>
+
+            } */}
+
+            {/* User dashboard check korar jonno testuserdashboard name route create kora ace. test sese ei route delete kora hobe  */}
+
             <AdminDashboard></AdminDashboard>
+            
         </div>
     );
 };
