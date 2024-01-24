931a87d066ac75e76fdeff92468b452fd6e28a51
isha35-1215 <tnishat652@gmail.com>
2024-01-24 20:00:05 +0600

    stripe adding

--- src/app/aboutUs/page.js
+++ src/app/aboutUs/page.js
@@ -1,12 +1,14 @@
 import Vision from '@/components/Vision/Vision';
 import OurValues from '@/components/OurValues/OurValues';
 import React from 'react';
+import Subscription from '@/components/Subscription/Subscription';
 
 const AboutUs = () => {
     return (
         <div>
             <Vision></Vision>
             <OurValues></OurValues>
+            <Subscription></Subscription>
         </div>
     );
 };
--- src/components/Subscribe/Subscribe.js
+++ src/components/Subscribe/Subscribe.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 const Subscribe = () => {
     return (
@@ -5,8 +6,8 @@ const Subscribe = () => {
             <h1 className="text-3xl md:text-4xl font-medium">Stay Informed, Stay Inspired!</h1>
             <p className="text-xl md:text-2xl py-6 md:px-20 ">Subscribe to Our Newsletter for Exclusive Updates, Insights, and Exciting Content.</p>
             <form className="mt-8 ">
-            <input type="text" placeholder="Type Your Email" className="input input-bordered border-[#E855DE] text-xl text-center md:text-left rounded-b-none md:rounded-md  input-md " />
-            <button className="btn bg-[#312B21] rounded-t-none md:rounded-md px-24 md:px-12 mx-4 md: text-2xl text-white font-medium">Subscribe</button>
+                <input type="text" placeholder="Type Your Email" className="input input-bordered border-[#E855DE] text-xl text-center md:text-left rounded-b-none md:rounded-md  input-md " />
+                <Link href="/subscription"><button className="btn bg-[#312B21] rounded-t-none md:rounded-md px-24 md:px-12 mx-4 md: text-2xl text-white font-medium">Subscribe</button></Link>
             </form>
         </div>
     );
