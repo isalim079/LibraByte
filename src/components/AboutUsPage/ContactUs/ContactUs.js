1cd1df4749dd6ed1438f77cdec9739a7601df944
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-01-25 22:18:03 +0600

    Contact page ongoing

--- src/components/AboutUsPage/ContactUs/ContactUs.js
+++ src/components/AboutUsPage/ContactUs/ContactUs.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import { FaLocationDot } from "react-icons/fa6";
-import { MdCall } from "react-icons/md";
+import { FiSend } from "react-icons/fi";
+import { IoLocation } from "react-icons/io5";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { FaPhoneAlt } from "react-icons/fa";
+import { BsBrowserChrome } from "react-icons/bs";
+
+
+
+
 const ContactUs = () => {
     return (
-        <div className=' mx-44 my-10 relative'>
-            <div className='bg-black h-[600px] p-10 flex gap-x-8 justify-center items-center '>
-                <div className='w-5/12  min-h-[450px] grid grid-cols-2 gap-8 absolute left-16 '>
-                    <div className='bg-white h-[220px] w-[220px]'>
-                    <FaLocationDot className='text-[50px] text-center mx-auto my-4'/>
-                    </div>
-                    <div className='bg-white h-[220px] w-[220px]'>
-                    <MdCall className='text-[50px] text-center mx-auto my-4' />
-                    </div>
-                    <div className='bg-white h-[220px] w-[220px]'>
+        <div className='my-10 mb-20 '>
+            <h1 className='text-4xl font-bold text-center'>Contact With Us </h1>
+            <p className='text-center text-lg pb-10 pt-2'>You can get our contact information and send us email</p>
+            <div className='bg-lightWhite mx-44 ml-80 min-h-[450px] absolute'>
 
-                    </div>
-                    <div className='bg-white h-[220px] w-[220px]'>
+                <h1 className='font-bold text-4xl text-darkOliveGreen pt-10 pl-56 pr-[500px]'>Get In Touch </h1>
+                <p className='text-xl  py-2 pl-56'>Fell free to drop us a comment </p>
 
+                <form >
+                    <div className='space-y-3'>
+                        <input type="text" placeholder="Name" className="input input-bordered w-full max-w-xl rounded-none bg-transparent shadow-xl ml-56" />
+                        <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xl rounded-none bg-transparent shadow-xl ml-56" />
+                        <textarea className="textarea textarea-bordered min-w-[575px] min-h-28 rounded-none bg-transparent shadow-xl ml-56" placeholder="Bio"></textarea>
                     </div>
+                    <button className='bg-oliveGreen text-white font-bold px-5 py-2 text-lg ml-56 flex justify-center items-center gap-x-3 mt-2'>Send <FiSend className='text-lg' />
+                    </button>
+                </form>
+
+            </div>
+
+            <div className='bg-oliveGreen min-h-[400px] w-80 relative left-44 top-6 '>
+                <h1 className='text-center font-bold text-white pt-7 text-xl'>Our Contact Information</h1>
+
+                <div className='p-5 space-y-4'>
+                    <h1><IoLocation className='text-white text-3xl' /> </h1>
+                    <h1><BiMessageSquareDetail className='text-white text-2xl' /> </h1>
+                    <h1><FaPhoneAlt className='text-white text-xl' /> </h1>
+                    <h1><BsBrowserChrome className='text-white text-xl' /> </h1>
                 </div>
-                <div className='w-7/12 bg-lightWhite min-h-[500px] absolute right-10 '>
 
-                </div>
             </div>
         </div>
     );
--- src/components/BookDetailsPage/BookDetails.js
+++ src/components/BookDetailsPage/BookDetails.js
@@ -4,11 +4,13 @@ import { FiArrowUpRight } from "react-icons/fi";
 import { GoBookmark } from "react-icons/go";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+
 
 
 const BookDetails = () => {
     return (
-        <div className='bg-lightWhite 2xl:h-[1200px] '>
+        <div className='bg-lightWhite 2xl:h-[1250px] '>
             <div className='min-h-[100vh] 2xl:relative'>
                 <div className='flex flex-col md:flex-row justify-center items-start px-5 xl:px-36 lg:px-24 md:px-14 2xl:px-44  py-2 2xl:py-20 '>
                     <div className='w-full 2xl:w-1/2 flex flex-col 2xl:flex-row justify-center pl-0 2xl:pl-36 items-center mr-10  h-3/4 z-30'>
@@ -42,7 +44,7 @@ const BookDetails = () => {
                         </div>
 
                     </div>
-                    <div className='p-10 2xl:p-24 flex flex-col md:flex-row justify-between items-center gap-x-20'>
+                    <div className='p-10 2xl:px-24 2xl:pt-24 flex flex-col md:flex-row justify-between items-center gap-x-20'>
                         <div className='w-full 2xl:w-6/12'>
                             <h1 className='font-bold text-xl pb-3'>Description</h1>
                             <p className='text-justify'> Facilis, laboriosam. Aliquid, est praesentium? Asperiores reprehenderit ex, in dignissimos nemo laborum expedita eligendi unde autem odio est eius quod? Dolor incidunt modi fuga cum, labore accusantium ab accusamus blanditiis, repellendus molestias totam odio .<br />laboriosam sint perspiciatis numquam incidunt dolor cumque, fugit suscipit ducimus natus assumenda saepe at. Molestiae, blanditiis temporibus  quae laboriosam maxime? Dolorem nesciunt provident dignissimos pariatur ullam fugiat fugit libero, repellendus assumenda corrupti neque laboriosam et accusamus sunt?</p>
@@ -67,6 +69,9 @@ const BookDetails = () => {
                             <h1 className='text-lg font-bold'>Paperback</h1>
                             <p className='text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium eum molestias similique cumque architecto et, dicta qui nobis porro expedita?</p>
                         </div>
+                    </div>
+                    <div className='flex justify-center items-center pb-10'>
+                        <button className='bg-oliveGreen text-white font-semibold text-lg px-10 py-2 rounded-3xl flex justify-between items-center gap-x-3'>Requst to borrow <BsFillRocketTakeoffFill className='text-xl'/></button>
                     </div>
                 </div>
             </div>
