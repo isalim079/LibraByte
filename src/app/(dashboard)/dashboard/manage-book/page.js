7f4bbb6fd95a7bdf501273714d1846c16f11f491
alif0013 <mdalifh36@gmail.com>
2024-01-26 00:56:30 +0600

    added manage book route

---
+++ src/app/(dashboard)/dashboard/manage-book/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <markquee><h1>Manage Book Page এ আপনাকে স্বাগতম। অনুগ্রহ করে চলে যান </h1></markquee>
+            
+        </div>
+    );
+};
+
+export default page;
--- src/components/Dashboard/AdminDashboard/RightSideContent/ProjectDetsilsTable/ProjectDetsilsTable.js
+++ src/components/Dashboard/AdminDashboard/RightSideContent/ProjectDetsilsTable/ProjectDetsilsTable.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import DetailsTable from './DetailsTable/DetailsTable';
+import Link from 'next/link';
 
 const ProjectDetsilsTable = () => {
     return (
         <div className='bg-white p-8 mt-5 drop-shadow-lg rounded-md'>
             <div className='flex justify-between items-center'>
                 <h1 className='text-2xl font-semibold mb-5'>Manage Books</h1>
-                <button className="bg-green-300 px-3 py-1 rounded-lg drop-shadow-md hover:bg-green-500 hover:cursor-pointer transition-all duration-300 ease-in-out hover:drop-shadow-2xl hover:text-white">View All »</button>
+                <Link href="dashboard/manage-book" className="bg-green-300 px-3 py-1 rounded-lg drop-shadow-md hover:bg-green-500 hover:cursor-pointer transition-all duration-300 ease-in-out hover:drop-shadow-2xl hover:text-white">View All »</Link>
             </div>
 
             <DetailsTable></DetailsTable>
