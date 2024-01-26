79e176641cc98890520b1aff6711e0a7365976a5
alif0013 <mdalifh36@gmail.com>
2024-01-27 01:31:12 +0600

    fixed layout issues

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,9 +1,25 @@
+import LeftSideMenu from "@/components/Dashboard/AdminDashboard/LeftSideMenu/LeftSideMenu";
+import HeaderText from "@/components/Dashboard/AdminDashboard/RightSideContent/HeaderText/HeaderText";
+import Searchbar from "@/components/Dashboard/AdminDashboard/RightSideContent/SearchBar/Searchbar";
 
-const DashboardLayout = ({children}) => {
+const DashboardLayout = ({ children }) => {
     return (
         <div>
 
-            {children}
+            <div className='grid grid-cols-1 md:grid-cols-12 lg:grid-cols-12 gap-1'>
+
+                <div className='md:col-span-3 lg:col-span-2 bg-lightWhite'>
+                    <LeftSideMenu></LeftSideMenu>
+                </div>
+
+                <div className='md:col-span-9 lg:col-span-10 px-2'>
+                    <Searchbar></Searchbar>
+                    <HeaderText></HeaderText>
+                    
+                    {children}
+                </div>
+            </div>
+          
 
         </div>
     );
