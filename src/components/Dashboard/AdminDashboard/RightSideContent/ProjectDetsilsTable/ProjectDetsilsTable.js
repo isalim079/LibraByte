4b433605cab32b058dbf89bf1c75149a750ac438
alif0013 <mdalifh36@gmail.com>
2024-01-27 23:55:07 +0600

    show dynamic username and image

--- src/components/Dashboard/AdminDashboard/RightSideContent/ProjectDetsilsTable/ProjectDetsilsTable.js
+++ src/components/Dashboard/AdminDashboard/RightSideContent/ProjectDetsilsTable/ProjectDetsilsTable.js
@@ -7,7 +7,7 @@ const ProjectDetsilsTable = () => {
         <div className='bg-white p-8 mt-5 drop-shadow-lg rounded-md'>
             <div className='flex justify-between items-center'>
                 <h1 className='text-2xl font-semibold mb-5'>Manage Books</h1>
-                <Link href="dashboard/manage-book" className="bg-green-300 px-3 py-1 rounded-lg drop-shadow-md hover:bg-green-500 hover:cursor-pointer transition-all duration-300 ease-in-out hover:drop-shadow-2xl hover:text-white">View All »</Link>
+                <Link href="/dashboard/manage-books" className="bg-oliveGreen text-white px-3 py-1 rounded-lg drop-shadow-md hover:bg-darkOliveGreen hover:cursor-pointer transition-all duration-300 ease-in-out hover:drop-shadow-2xl hover:text-white">View All »</Link>
             </div>
 
             <DetailsTable></DetailsTable>
