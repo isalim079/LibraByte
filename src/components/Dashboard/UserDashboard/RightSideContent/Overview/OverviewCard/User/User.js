cc285ed9f342a026e4d2e783c74b11c26246d597
isalim079 <salimreza1025@gmail.com>
2024-02-01 19:40:38 +0600

    dashboard edited

--- src/components/Dashboard/UserDashboard/RightSideContent/Overview/OverviewCard/User/User.js
+++ src/components/Dashboard/UserDashboard/RightSideContent/Overview/OverviewCard/User/User.js
@@ -1,25 +1,35 @@
+"use client"
+
+import { AuthContext } from '@/app/Context/AuthProvider';
+import useFindUser from '@/lib/hooks/useFindUser';
 import Image from 'next/image';
-import React from 'react';
+import React, { useContext } from 'react';
 
 const User = () => {
+
+    const {user} = useContext(AuthContext)
+
+    const [findUser] = useFindUser()
+    // console.log(findUser);
+
     return (
         <div className='bg-white drop-shadow-md rounded-md p-4'>
 
-            <div className='flex justify-between items-center md:gap-1'>
+            <div className='flex flex-row-reverse justify-between items-center md:gap-1'>
 
                 <div>
-                    <h3 className='text-xl font-semibold'>Userss</h3>
+                    <h3 className='text-xl font-semibold'>{findUser?.name}</h3>
 
                     <div className='flex py-1 items-center gap-3'>
-                        <h2 className='font-bold md:text-xl'>103</h2>
-                        <span className='text-[#68B8A1] font-bold text-[13px]'>+5.2%</span>
+                        <h2 className=' md:text-sm'>{findUser?.role}</h2>
+                        <span className='text-[#68B8A1] font-bold text-sm'></span>
                     </div>
 
-                    <p className='text-[#9da1a4] text-[13px]'>Active Users</p>
+                    <p className='text-[#9da1a4] text-[13px]'>Active {findUser?.role === "admin" ? "admin" : "user"}</p>
                 </div>
  
                 <div>
-                    <Image height={40} width={40} src="https://i.ibb.co/2Z1xWdH/user-3177440.png"></Image>
+                    <Image className='rounded-full' height={60} width={60} src={user?.photoURL}></Image>
                 </div>
 
             </div>
