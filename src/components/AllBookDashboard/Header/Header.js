396fde47d39e57019b6468ea51d79e445f70e2c3
isalim079 <salimreza1025@gmail.com>
2024-02-03 09:08:32 +0600

    searchBar commented

--- src/components/AllBookDashboard/Header/Header.js
+++ src/components/AllBookDashboard/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
             <div className="drop-shadow-md px-5 py-2 bg-lightWhite ">
                 <div className=" flex justify-between items-center gap-2">
                     <div className="form-control ">
-                        <input type="text" placeholder="Search your favorite book" className="input input-bordered w-24 md:w-auto" />
+                        {/* <input type="text" placeholder="Search your favorite book" className="input input-bordered w-24 md:w-auto" /> */}
                     </div>
                     <div className=" flex justify-center items-center">
                         <Novu></Novu>
