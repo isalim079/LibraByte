7256941fb550ee0af69b87c7eac4211bcc47af42
isalim079 <salimreza1025@gmail.com>
2024-02-07 09:45:29 +0600

    top post section design and color updated

--- src/components/Blog/LeftSideSection/TopPost/TopPost.js
+++ src/components/Blog/LeftSideSection/TopPost/TopPost.js
@@ -20,12 +20,12 @@ const TopPost = () => {
     // console.log(topPostInfo);
 
     return (
-        <div className="mt-8 bg-lightWhite p-8 rounded-md h-[500px] overflow-y-auto drop-shadow-lg">
-            <div>
-                <h2 className="text-2xl font-semibold text-oliveGreen">
+        <div className="mt-8 bg-royalBlue p-8 rounded-md h-[500px] overflow-y-auto drop-shadow-lg">
+            <div className="">
+                <h2 className="text-2xl font-semibold text-slate-200">
                     Top Post
                 </h2>
-                <div className="border-2 border-oliveGreen w-[102px] mt-1 mb-5"></div>
+                <div className="border-2 border-slate-400 w-[102px] mt-1 mb-5"></div>
             </div>
             <div className="list-none">
                 <div className="">
@@ -33,20 +33,20 @@ const TopPost = () => {
                         <div key={index}>
                             <div className="flex items-center space-x-6">
                                 <Image
-                                    className="rounded-full h-[50px]"
+                                    className="rounded-full h-[50px] border-2 border-customYellow"
                                     src={topPost?.image}
                                     width={50}
                                     height={100}
                                     alt="image"
                                 ></Image>
-                                <p className="font-semibold">{topPost?.name}</p>
+                                <p className="font-semibold text-slate-200">{topPost?.name}</p>
                             </div>
                             <div className="mt-4 mb-10">
-                                <h3 className="text-lg font-semibold">
+                                <h3 className="text-lg font-semibold text-slate-200">
                                     {topPost?.blogPostTitle}
                                 </h3>
 
-                                <p className="text-justify mt-1 text-textLightWhite">
+                                <p className="text-justify mt-1 text-slate-400">
                                     {topPost?.description}
                                 </p>
                             </div>
