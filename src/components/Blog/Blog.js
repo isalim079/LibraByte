e2733132a6c9cfcf2cf0c608188137a074604efa
isalim079 <salimreza1025@gmail.com>
2024-02-07 09:39:43 +0600

    blog category section design and color updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,7 +7,8 @@ import RightSideSection from "./RightSideSection/RightSideSection";
 
 const Blog = () => {
     return (
-        <div className="max-w-screen-xl mx-auto pt-24 md:pt-28 mb-10 overflow-x-hidden">
+        <div className="bg-bgTexture">
+            <div className="max-w-screen-xl mx-auto pt-24 md:pt-28 mb-10 overflow-x-hidden">
             {/* Full blog container */}
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 ">
@@ -27,6 +28,7 @@ const Blog = () => {
                     <RightSideSection />
                 </div>
             </div>
+        </div>
         </div>
     );
 };
