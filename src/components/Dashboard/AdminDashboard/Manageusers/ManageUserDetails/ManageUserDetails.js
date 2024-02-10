d9245059897df04b06d6c95cae1f44f02fe25847
isalim079 <salimreza1025@gmail.com>
2024-02-10 15:58:03 +0600

    user delete function added

--- src/components/Dashboard/AdminDashboard/Manageusers/ManageUserDetails/ManageUserDetails.js
+++ src/components/Dashboard/AdminDashboard/Manageusers/ManageUserDetails/ManageUserDetails.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import useAxiosPublic from "@/lib/hooks/useAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
@@ -6,21 +6,18 @@ import { AiOutlineMail } from "react-icons/ai";
 import Swal from "sweetalert2";
 
 const ManageUserDetails = async () => {
-
-    const axiosPublic = useAxiosPublic()
-
+    const axiosPublic = useAxiosPublic();
 
     /* getting all users */
-    const {data = [], refetch} = useQuery({
-        queryKey: ['usersData'],
+    const { data = [], refetch } = useQuery({
+        queryKey: ["usersData"],
         queryFn: async () => {
-            const usersData = await axiosPublic.get('/users/v1')
-            return usersData.data
-        }
-    })
-    const usersData = data
+            const usersData = await axiosPublic.get("/users/v1");
+            return usersData.data;
+        },
+    });
+    const usersData = data;
     // console.log(usersData);
-
 
     /* changing user role */
     const handleMakeAdminUser = (users) => {
@@ -33,63 +30,84 @@ const ManageUserDetails = async () => {
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
-            confirmButtonText: "Yes!"
-          }).then( async(result) => {
+            confirmButtonText: "Yes!",
+        }).then(async (result) => {
             if (result.isConfirmed) {
-
-
-                    /* making user to admin */
-               if(users.role === "user") {
-                const res = await axiosPublic.patch(
-                    `/userRole/v1/${users._id}`,
-                    {
-                        role: "admin",
+                /* making user to admin */
+                if (users.role === "user") {
+                    const res = await axiosPublic.patch(
+                        `/userRole/v1/${users._id}`,
+                        {
+                            role: "admin",
+                        }
+                    );
+                    // console.log(res);
+                    if (res.status === 200) {
+                        refetch();
+                        Swal.fire({
+                            title: "You have successfully updated this user role!",
+                            text: "Role updated.",
+                            icon: "success",
+                        });
                     }
-                );
-                // console.log(res);
-                if (res.status === 200) {
-                    refetch();
-                    Swal.fire({
-                        title: "You have successfully updated this user role!",
-                        text: "Role updated.",
-                        icon: "success"
-                      });
                 }
-               }
 
-               /* making admin to user */
-               if(users.role === "admin") {
-                const res = await axiosPublic.patch(
-                    `/userRole/v1/${users._id}`,
-                    {
-                        role: "user",
+                /* making admin to user */
+                if (users.role === "admin") {
+                    const res = await axiosPublic.patch(
+                        `/userRole/v1/${users._id}`,
+                        {
+                            role: "user",
+                        }
+                    );
+                    // console.log(res);
+                    if (res.status === 200) {
+                        refetch();
+                        Swal.fire({
+                            title: "You have successfully updated this user role!",
+                            text: "Role updated.",
+                            icon: "success",
+                        });
                     }
-                );
-                // console.log(res);
-                if (res.status === 200) {
-                    refetch();
-                    Swal.fire({
-                        title: "You have successfully updated this user role!",
-                        text: "Role updated.",
-                        icon: "success"
-                      });
                 }
-               }
-
-
-
-             
             }
-          });
+        });
+    };
 
-    }
+    /* delete user */
+    const handleDeleteUser = (users) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete this user!",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosPublic.delete(
+                    `/userDelete/v1/${users._id}`
+                );
+                if (res.status === 200) {
+                    refetch();
+
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "User has been deleted.",
+                        icon: "success",
+                    });
+                }
+            }
+        });
+    };
 
     return (
         <div className="mt-7">
             <div className="overflow-x-auto">
                 <table className="table table-md">
                     <thead>
-                        <tr className='bg-royalBlue text-white text-[14px] drop-shadow-md'>
+                        <tr className="bg-royalBlue text-white text-[14px] drop-shadow-md">
                             <th>#</th>
                             <th>User Name</th>
                             <th>User Email</th>
@@ -101,29 +119,42 @@ const ManageUserDetails = async () => {
                         </tr>
                     </thead>
                     <tbody>
-
-                        {
-                            usersData.map((user, idx) => <>
+                        {usersData.map((user, idx) => (
+                            <>
                                 <tr key={user._id}>
                                     <th>{idx + 1}</th>
                                     <td>{user?.name}</td>
                                     <td>{user?.email}</td>
-                                    <td><AiOutlineMail className="text-xl"></AiOutlineMail> </td>
+                                    <td>
+                                        <AiOutlineMail className="text-xl"></AiOutlineMail>{" "}
+                                    </td>
                                     <td>{user?.date}</td>
                                     <td>{user?.subscription}</td>
                                     <td>{user?.role}</td>
                                     <td className="flex gap-3 justify-between">
-                                        <button onClick={() => handleMakeAdminUser(user)} className="hover:shadow-lightBtn hover:shadow-md flex justify-center items-center bg-lightBtn text-white hover:bg-darkBtn md:px-2 md:py-1 btn-xs mr-1">{user?.role === "admin" ? "Make User" : "Make Admin"}</button>
-                                        <button className="hover:shadow-red-700 hover:shadow-md flex justify-center items-center bg-red-600 md:px-2 md:py-1 hover:bg-red-700 text-white  btn-xs">Delete</button>
+                                        <button
+                                            onClick={() =>
+                                                handleMakeAdminUser(user)
+                                            }
+                                            className="hover:shadow-lightBtn hover:shadow-md flex justify-center items-center bg-lightBtn text-white hover:bg-darkBtn md:px-2 md:py-1 btn-xs mr-1"
+                                        >
+                                            {user?.role === "admin"
+                                                ? "Make User"
+                                                : "Make Admin"}
+                                        </button>
+                                        <button
+                                            onClick={() =>
+                                                handleDeleteUser(user)
+                                            }
+                                            className="hover:shadow-red-700 hover:shadow-md flex justify-center items-center bg-red-600 md:px-2 md:py-1 hover:bg-red-700 text-white  btn-xs"
+                                        >
+                                            Delete
+                                        </button>
                                     </td>
-
                                 </tr>
-                            </>)
-                        }
-
-
+                            </>
+                        ))}
                     </tbody>
-
                 </table>
             </div>
         </div>
