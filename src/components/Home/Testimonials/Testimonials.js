30e1bcd0c3b6f64c515362312d94239402a422d1
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2024-03-07 04:49:25 +0600

    Testimonial responsive issue solve

--- src/components/Home/Testimonials/Testimonials.js
+++ src/components/Home/Testimonials/Testimonials.js
@@ -77,28 +77,29 @@ const Testimonials = () => {
                 <Slider ref={sliderRef} {...settings} className="my-auto flex items-center">
                     {reviews.map((review) => (
                         <div key={review._id} className="slick-slide ">
-                            <div className="card overflow-y-auto mx-4 px-4 bg-slate-200 rounded-box py-6">
+                            <div className="card overflow-y-auto mx-2 md:mx-4 px-1 md:px-4  bg-slate-200 rounded-box py-6 ">
                                 <div>
                                     <div className="">
                                         <img
-                                            className="rounded-full mx-auto w-28 h-28 flex items-center justify-center"
+                                            className="rounded-full mx-auto w-16 h-16 md:w-28 md:h-28 flex items-center justify-center"
                                             src={review?.image}
                                             alt="userPhoto"
                                         />
                                     </div>
                                 </div>
-                                <h2 className="text-xl font-semibold text-center py-3">{review?.name}</h2>
-                               <div className="flex justify-center">
-                               <Rating
-                        className='space-x-2 text-xl text-yellow-500'
-            initialRating={review?.rating}
-            // onClick={onRatingSelected}
-            stop={5}
-            emptySymbol={<BsStar className=''></BsStar>}
-            fullSymbol={<BsStarFill className=""></BsStarFill>}
-          />
-                               </div>
-                                <h1 className=" text-base px-4 md:px-10 mx-auto py-3">{review?.review}</h1>
+                                <h2 className="text-md md:text-xl font-normal  md:font-semibold text-center py-3">{review?.name}</h2>
+                                <div className="flex justify-center">
+                                    <Rating
+                                        className='space-x-2 text-md md:text-xl text-yellow-500'
+                                        initialRating={review?.rating}
+                                        // onClick={onRatingSelected}
+                                        stop={5}
+                                        emptySymbol={<BsStar className=''></BsStar>}
+                                        fullSymbol={<BsStarFill className=""></BsStarFill>}
+                                    />
+                                </div>
+                                <h1 className=" text-xs px-4 md:px-10 mx-auto py-3 hidden md:block">{review?.review}</h1>
+                                <h1 className=" text-base px-4 md:px-10 mx-auto py-3 md:hidden">{review?.review.slice(0, 70)}</h1>
                             </div>
                         </div>
                     ))}
@@ -110,4 +111,4 @@ const Testimonials = () => {
 
 // export default Testimonials;
 
-export default dynamic(() => Promise.resolve(Testimonials), {ssr: false})
+export default dynamic(() => Promise.resolve(Testimonials), { ssr: false })
