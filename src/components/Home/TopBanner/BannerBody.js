fbd1157960e2305fa8851d1a248acd4e869e4495
isalim079 <salimreza1025@gmail.com>
2024-03-07 07:55:27 +0600

    top banner responsive done

--- src/components/Home/TopBanner/BannerBody.js
+++ src/components/Home/TopBanner/BannerBody.js
@@ -10,10 +10,10 @@ const BannerBody = ({bannerDetails}) => {
     return (
         <div className="max-w-screen-xl mx-auto md:pt-20 p-3 ">
             <div className="flex md:flex-row flex-col justify-between items-center ">
-                <div className="">
-                    <h1 className="text-slate-300 font-medium lg:mb-5 bg-royalBlue/70 rounded-md p-4 text-2xl lg:text-3xl uppercase border-b-[3px] border-b-customYellow text-center">{bannerDetails?.bookName}</h1>
+                <div className="md:w-full md:max-w-[50%] lg:max-w-[57%]">
+                    <h1 className="text-slate-300 font-medium mb-3 lg:mb-5 bg-royalBlue/70 rounded-md p-2 lg:p-4 text-xl lg:text-3xl uppercase border-b-[3px] border-b-customYellow text-center">{bannerDetails?.bookName}</h1>
                     {/* <div className="border-2 border-customYellow mb-4"></div> */}
-                    <p className="text-slate-200 md:w-[720px] p-8 text-justify md:text-base font-montserrat text-sm bg-royalBlue/70 rounded-md border-b-[3px] border-b-white hover:bg-royalBlue/80">
+                    <p className="text-slate-200 p-8 text-justify md:text-base font-montserrat text-xs bg-royalBlue/70 rounded-md border-b-[3px] border-b-white hover:bg-royalBlue/80">
                         {bannerDetails?.description}
                     </p>
                 </div>
