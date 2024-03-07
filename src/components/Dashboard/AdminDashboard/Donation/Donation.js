a998eaf254d9762d310433dee669b0ba1b91ec7d
nadira1187 <nadira35-1187@s.diu.edu.bd>
2024-03-07 22:42:05 +0600

    added patch operation in donation route of admin

--- src/components/Dashboard/AdminDashboard/Donation/Donation.js
+++ src/components/Dashboard/AdminDashboard/Donation/Donation.js
@@ -2,10 +2,15 @@
 import axios from 'axios';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const Donation = () => {
     const [books,setBooks]=useState([]);
     useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const fetchBooks = () => {
         axios
             .get("http://localhost:5000/ngo-donate/v1")
             .then((res) => {
@@ -14,7 +19,30 @@ const Donation = () => {
             .catch((error) => {
                 console.log("error getting donation", error);
             });
-    }, []);
+    };
+
+    const handleAccept = (id) => {
+        axios
+            .patch(`http://localhost:5000/ngo-donate/v1/${id}`, { isAccept: true })
+            .then(() => {
+                fetchBooks(); 
+                toast.success("Book is accepted")
+            })
+            .catch((error) => {
+                console.log("error accepting book", error);
+            });
+    };
+
+    // const handleRemove = (id) => {
+    //     axios
+    //         .delete(`http://localhost:5000/ngo-donate/v1/${id}`)
+    //         .then(() => {
+    //             fetchBooks(); // Refresh the book list after deleting
+    //         })
+    //         .catch((error) => {
+    //             console.log("error removing book", error);
+    //         });
+    // };
     return (
         <div>
             
@@ -26,7 +54,7 @@ const Donation = () => {
                             <th>Book Name</th>
                             <th>Author Name</th>
                             <th>Category</th>
-                            <th>Remove</th>
+                            {/* <th>Remove</th> */}
                             <th>Accept</th>
                         </tr>
                     </thead>
@@ -60,15 +88,21 @@ const Donation = () => {
                                         {/* <span className="badge badge-ghost badge-sm"></span> */}
                                     </td>
                                     <td>{book.category}</td>
-                                    <th>
+                                    {/* <th>
                                         <button className="btn bg-red-500 text-white md:px-2 md:py-1 hover:bg-red-600 hover:text-white  btn-xs">
                                             Delete
                                         </button>
-                                    </th>
+                                    </th> */}
                                     <th>
-                                        <button className="btn bg-green-500 text-white md:px-2 md:py-1 hover:bg-green-600 hover:text-white  btn-xs">
-                                            Add
+                                    {book.isAccept ? (
+                                        <button className="btn bg-green-500 text-white md:px-2 md:py-1 hover:bg-green-600 hover:text-white  btn-xs" disabled>
+                                            Accepted
                                         </button>
+                                    ) : (
+                                        <button className="btn bg-green-500 text-white md:px-2 md:py-1 hover:bg-green-600 hover:text-white  btn-xs" onClick={() => handleAccept(book._id)}>
+                                            Accept
+                                        </button>
+                                    )}
                                     </th>
                                 </tr>
                             </>
